8504305999e1c93c3e675693aa869da541ee2549
Yarik091121 <sergeevyarik7@gmail.com>
2024-12-24 13:59:39 +0300

    create master-branch

---
+++ bro.config.js
@@ -0,0 +1,22 @@
+const pkg = require("./package");
+
+module.exports = {
+  apiPath: "stubs/api",
+  webpackConfig: {
+    output: {
+      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`,
+    },
+  },
+  /* use https://admin.bro-js.ru/ to create config, navigations and features */
+  navigations: {
+    "artcollab.main": "/artcollab",
+  },
+  features: {
+    "artcollab": {
+      // add your features here in the format [featureName]: { value: string }
+    },
+  },
+  config: {
+    "artcollab.api": "/api",
+  },
+};
