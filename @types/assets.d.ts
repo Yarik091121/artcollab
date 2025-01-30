2683bbf6463f5405f7e822d6db7b13b49b640a15
Yarik091121 <sergeevyarik7@gmail.com>
2025-01-31 00:44:02 +0300

    feat: add jpg and gif modules

--- @types/assets.d.ts
+++ @types/assets.d.ts
@@ -3,3 +3,18 @@ declare module '*.png' {
   
     export default imagePath;
 }
+declare module '*.jpg' {
+    const jpgPath: string;
+  
+    export default jpgPath;
+}
+declare module '*.jpeg' {
+    const jpegPath: string;
+  
+    export default jpegPath;
+}
+declare module '*.gif' {
+    const gifPath: string;
+
+    export default gifPath;
+}
